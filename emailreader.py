7479160581e33d19e9843df25743abd5365b160f
Diogosoaresds <90044752+Diogosoaresds@users.noreply.github.com>
2024-03-27 11:01:27 -0300

    Update emailreader.py

--- emailreader.py
+++ emailreader.py
@@ -14,9 +14,7 @@ def filtrar_planilha(caminho_arquivo, mes_ano_inicio, mes_ano_fim):
         for row in sheet.iter_rows(min_row=2, values_only=True):
             data_row = row[4]  # Data na coluna 5 (index 4)
             if data_row is not None:
-                # Extrair o mês e o ano da célula da data na planilha
                 mes_ano_data = data_row.strftime('%m-%Y')
-                # Comparar com o período fornecido pelo usuário
                 if mes_ano_inicio <= mes_ano_data <= mes_ano_fim:
                     linhas_filtradas.append(row)
 
@@ -28,7 +26,6 @@ def filtrar_planilha(caminho_arquivo, mes_ano_inicio, mes_ano_fim):
 
 def enviar_email(destinatario):
     try:
-        # Configurações do servidor SMTP
         smtp_server = 'smtp.gmail.com'
         port = 587
         sender_email = 'diogo.soaresdas9@gmail.com'
@@ -36,22 +33,18 @@ def enviar_email(destinatario):
         assunto = "Teste"
         mensagem = "Olá,\n\nEsta é uma mensagem de teste."
 
-        # Criar objeto de e-mail
         msg = MIMEMultipart()
         msg['From'] = sender_email
         msg['To'] = destinatario
         msg['Subject'] = assunto
         msg.attach(MIMEText(mensagem, 'plain'))
 
-        # Estabelecer conexão com o servidor SMTP
         server = smtplib.SMTP(smtp_server, port)
         server.starttls()
         server.login(sender_email, password)
 
-        # Enviar e-mail
         server.send_message(msg)
         
-        # Fechar conexão
         server.quit()
         
         print(f"E-mail enviado para {destinatario} com sucesso!")
@@ -63,11 +56,9 @@ if __name__ == "__main__":
     data_inicio = input("Digite a data de início (formato DD-MM-AAAA): ")
     data_fim = input("Digite a data de fim (formato DD-MM-AAAA): ")
 
-    # Filtrar planilha
     linhas_filtradas = filtrar_planilha(caminho_arquivo, data_inicio, data_fim)
 
     if linhas_filtradas:
-        # Enviar e-mails para cada destinatário
         for linha in linhas_filtradas:
             destinatario = linha[7]  # Destinatário na coluna 8
             enviar_email(destinatario)
